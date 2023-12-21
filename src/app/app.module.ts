6d6860ef26300b213cfb0144ce036e455e8bd7bd
TAC42 <tomi.carlin@gmail.com>
2023-12-21 11:56:50 +0200

    created home page cmp and fixed app-root cmp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-root/app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './cmps/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule, 
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+// import { Subscription, switchMap, take } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  // private userService = inject(UserService)
+  // private bitcoinService = inject(BitcoinService)
+  // subscription!: Subscription
+  // firstUser: User | null = null
+
+  // bitCoinRate$ = this.bitcoinService.getBitCoinRate()
+  // user = this.userService.getUser()
+  // user$ = this.userService.getUserObs(this.user)
+  // bitCoinRate2$ = this.user$.pipe(
+  //   switchMap(user => this.bitcoinService.getBitCoinRateStream(user.coins))
+  // )
+
+
+
+  // ngOnInit(): void {
+  //   this.subscription = this.userService.query().pipe(take(1)).subscribe({
+  //     next: users => {
+  //       this.firstUser = users[0];
+  //     },
+  //     error: err => console.log('err:', err)
+  //   });
+  // }
+
+  // ngOnDestroy(): void {
+  //   this.subscription?.unsubscribe?.()
+  // }
+}
+
+
+
+
