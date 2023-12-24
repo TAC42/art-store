ded498d3dfe0c013c074077fbd2705f0f4edd09a
TAC42 <tomi.carlin@gmail.com>
2023-12-24 13:28:31 +0200

    created shop.service! to handle crud for the products

---
+++ src/app/models/shop.ts
@@ -0,0 +1,14 @@
+export interface Product {
+    name: string;
+    imgUrl: string;
+    price: number;
+    description: string;
+    inStock: boolean;
+    createdAt: number;
+    _id: string;
+  }
+  
+  export interface ShopFilter {
+    search: string;
+  }
+  
