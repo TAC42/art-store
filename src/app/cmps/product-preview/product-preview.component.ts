a69cbc9ae9791d80909856547b9d54c2a4bc16fd
TAC42 <tomi.carlin@gmail.com>
2023-12-24 23:00:21 +0200

    fixed function.scss deprecated issue

--- src/app/cmps/product-preview/product-preview.component.ts
+++ src/app/cmps/product-preview/product-preview.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Product } from '../../models/shop';
-import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { Product } from '../../models/shop'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'product-preview',
@@ -9,25 +9,17 @@ import { HttpClient } from '@angular/common/http';
 export class ProductPreviewComponent {
   @Input() product!: Product
   @Output() remove = new EventEmitter()
-  prodImg: string = '';
+  prodImg: string = ''
 
   constructor(private http: HttpClient) {
-    this.fetchRandomRobotImage();
+    this.fetchRandomImage()
   }
 
-  fetchRandomRobotImage() {
-    this.http.get<any>('https://robohash.org/').subscribe(
-      (data) => {
-        // Data will contain a random robot image URL
-        this.prodImg = data?.imageUrl || 'default_image_url_if_not_available';
-      },
-      (error) => {
-        console.error('Error fetching robot image:', error);
-        // Set a default image URL in case of an error
-        this.prodImg = 'default_image_url';
-      }
-    );
+  fetchRandomImage() {
+    const uniqueId = Math.floor(Math.random() * 1000)
+    this.prodImg = `https://picsum.photos/100/150?random=${uniqueId}`
   }
+
   onRemoveProduct() {
     this.remove.emit(this.product._id)
   }
