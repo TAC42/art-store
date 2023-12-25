a379386aff1f38a213328ab5fad32ea0961ecd93
Valeriy Kuvshinov <valerykuvshinuv@gmail.com>
2023-12-25 14:39:31 +0200

    aside menu animations

--- src/app/cmps/aside-menu/aside-menu.component.ts
+++ src/app/cmps/aside-menu/aside-menu.component.ts
@@ -1,8 +1,20 @@
 import { Component, ElementRef, HostListener } from '@angular/core'
+import { trigger, style, animate, transition } from '@angular/animations'
 
 @Component({
   selector: 'app-aside-menu',
-  templateUrl: './aside-menu.component.html'
+  templateUrl: './aside-menu.component.html',
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('500ms ease-in-out', style({ transform: 'translateX(0%)' }))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in-out', style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
 })
 
 export class AsideMenuComponent {
@@ -15,14 +27,18 @@ export class AsideMenuComponent {
     this.isMenuOpen = !this.isMenuOpen
   }
 
+  get menuState() {
+    return this.isMenuOpen ? 'in' : 'out'
+  }
+
   closeMenu() {
     this.isMenuOpen = false
   }
 
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent): void {
-    const clickedInside = this.elementRef.nativeElement.contains(event.target)
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
 
-    if (!clickedInside) this.isMenuOpen = false
+    if (!clickedInside) this.closeMenu()
   }
 }
