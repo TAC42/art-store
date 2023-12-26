19077fab2ad08af703ff5c11d66245e1522df08c
Valeriy Kuvshinov <valerykuvshinuv@gmail.com>
2023-12-26 13:11:27 +0200

    smoother image carousel

--- frontend/src/app/cmps/image-carousel/image-carousel.component.ts
+++ frontend/src/app/cmps/image-carousel/image-carousel.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core'
+import { trigger, style, animate, transition } from '@angular/animations'
 
 @Component({
   selector: 'app-image-carousel',
-  templateUrl: './image-carousel.component.html'
+  templateUrl: './image-carousel.component.html',
 })
 
 export class ImageCarouselComponent {
@@ -15,7 +16,21 @@ export class ImageCarouselComponent {
   }
 
   previousImage(): void {
-    this.currentIndex = this.currentIndex > 0
-      ? this.currentIndex - 1 : this.imageUrls.length - 1
+    if (this.currentIndex === 0) {
+      this.currentIndex = this.imageUrls.length - 1
+    } else this.currentIndex--
+  }
+
+  getSlideStyle(index: number): object {
+    let offset = (index - this.currentIndex) * 100
+
+    if (this.currentIndex === 0 && index === this.imageUrls.length - 1) {
+      offset = -100
+    } else if (this.currentIndex === this.imageUrls.length - 1 && index === 0) {
+      offset = 100
+    }
+
+    const opacity = index === this.currentIndex ? 1 : 0
+    return { 'transform': `translateX(${offset}%)`, 'opacity': opacity }
   }
 }
