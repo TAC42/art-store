59a548da81cc91081c25e59791ef362b5a4d7099
TAC42 <tomi.carlin@gmail.com>
2023-12-28 12:27:32 +0200

    small error in routing with resolver

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,9 +3,18 @@ import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component'
 import { AboutComponent } from './pages/about/about.component'
 import { ShopComponent } from './pages/shop/shop.component'
+import { ProductEditComponent } from './cmps/product-edit/product-edit.component'
+import { ProductDetailsComponent } from './cmps/product-details/product-details.component'
+import { ProductResolver } from './resolvers/product.resolver'
 
 const routes: Routes = [
-  { path: 'shop', component: ShopComponent },
+  { 
+    path: 'shop', component: ShopComponent, children: [
+      { path: ':name', component: ProductDetailsComponent, resolve: { product: ProductResolver } },
+      { path: 'edit/:name', component: ProductEditComponent },
+      { path: 'edit', component: ProductEditComponent },
+  ]
+},
   { path: 'about', component: AboutComponent },
   { path: '', component: HomeComponent },
 ]
