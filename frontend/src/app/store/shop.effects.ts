80092e1b3cf73df54988b83506dcee8a72244ce6
TAC42 <tomi.carlin@gmail.com>
2023-12-29 23:20:06 +0200

    added loader logic to shop loading

--- frontend/src/app/store/shop.effects.ts
+++ frontend/src/app/store/shop.effects.ts
@@ -1,28 +1,30 @@
 import { Injectable } from '@angular/core'
 import { Actions, createEffect, ofType } from '@ngrx/effects'
-import { map, mergeMap } from 'rxjs/operators'
+import { map, mergeMap, tap } from 'rxjs/operators'
 import { ShopDbService } from '../services/shop-db.service'
 import { loadProducts, productsLoaded } from './shop.actions'
+import { LoaderService } from '../services/loader.service'
 
 @Injectable()
 export class ShopEffects {
+  constructor(
+    private actions$: Actions,
+    private shopDbService: ShopDbService,
+    private loaderService: LoaderService // Inject the LoaderService
+  ) {}
 
-    loadProducts$ = createEffect(() =>
-        this.actions$.pipe(
-            ofType(loadProducts),
-            mergeMap(({ filterBy }) =>
-                this.shopDbService.query(filterBy).pipe( // Pass the filter to your query method
-                    map((products) => {
-                        console.log('products: ',products)
-                       return productsLoaded({ products })
-                    }
-                    ),
-                )
-            )
+  loadProducts$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadProducts),
+      tap(() => this.loaderService.setIsLoading(true)), // Start the loader
+      mergeMap(({ filterBy }) =>
+        this.shopDbService.query(filterBy).pipe(
+          map((products) => productsLoaded({ products })),
+          tap(() => this.loaderService.setIsLoading(false)) // Stop the loader after products are loaded
         )
+      )
     )
+  )
 
-    // Other effects for add, update, remove, etc., can be added similarly
-
-    constructor(private actions$: Actions, private shopDbService: ShopDbService) { }
+  // Other effects for add, update, remove, etc., can be added similarly
 }
