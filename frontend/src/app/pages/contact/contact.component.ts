2a953055c056dbdf72e0b809a4eea91592d8a5b8
Valeriy Kuvshinov <valerykuvshinuv@gmail.com>
2023-12-30 16:00:14 +0200

    initial form in contact page

--- frontend/src/app/pages/contact/contact.component.ts
+++ frontend/src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core'
 })
 
 export class ContactComponent {
+  loneImg: string = 'https://res.cloudinary.com/dv4a9gwn4/image/upload/v1703511845/Gallery/Function/zhyc1jbekytmh92gjdea.png'
 
+  contactForm: FormGroup
+
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      title: ['', Validators.required],
+      message: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      // Code to handle form submission, like sending an email
+      console.log(this.contactForm.value)
+    }
+  }
 }
