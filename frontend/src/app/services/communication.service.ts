d28cb93faecd946c13d9c56c82f392f26cd52fd9
TAC42 <tomi.carlin@gmail.com>
2024-01-10 22:33:28 +0200

    minor changes to app-root-html and communication services

--- frontend/src/app/services/communication.service.ts
+++ frontend/src/app/services/communication.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Injectable } from '@angular/core'
+import { Subject } from 'rxjs'
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommunicationService {
-  private removeProductSubject = new Subject<string>();
-  removeProduct$ = this.removeProductSubject.asObservable();
+  private removeProductSubject = new Subject<string>()
+  removeProduct$ = this.removeProductSubject.asObservable()
 
   emitRemoveProduct(productId: string) {
-    this.removeProductSubject.next(productId);
+    this.removeProductSubject.next(productId)
   }
 }
