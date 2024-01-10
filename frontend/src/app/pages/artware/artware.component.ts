bd8a093f5d52291bfedd98dafd07a9e4edebf88b
TAC42 <tomi.carlin@gmail.com>
2024-01-10 13:13:57 +0200

    created sculpture and artware as well as working on shop-index filter

---
+++ frontend/src/app/pages/artware/artware.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'artware',
+  templateUrl: './artware.component.html'
+})
+export class ArtwareComponent {
+
+}
---
+++ frontend/src/app/pages/sculpture/sculpture.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sculpture',
+  templateUrl: './sculpture.component.html'
+})
+export class SculptureComponent {
+
+}
