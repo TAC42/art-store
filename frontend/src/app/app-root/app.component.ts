00af155d9e08bb4144dd2e0bf63c4a87ab9f928a
Valeriy Kuvshinov <valerykuvshinuv@gmail.com>
2024-01-24 00:45:11 +0200

    further user-auth modal adjustments and security

--- frontend/src/app/app-root/app.component.ts
+++ frontend/src/app/app-root/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core'
-import { Store } from '@ngrx/store'
+import { select, Store } from '@ngrx/store'
 import { AppState } from '../store/app.state'
 import { SET_LOGGEDIN_USER, LOGOUT } from '../store/user.actions'
 import { DimmerService } from '../services/dimmer.service'
 import { filter, Subscription } from 'rxjs'
 import { NavigationEnd, Router } from '@angular/router'
+import { ModalService } from '../services/modal.service'
+import { selectLoggedinUser } from '../store/user.selectors'
 
 const SESSION_KEY_LOGGEDIN_USER = 'loggedinUser'
 
@@ -17,10 +19,12 @@ export class AppComponent implements OnInit, OnDestroy {
   private store = inject(Store<AppState>)
   private router = inject(Router)
   private dimService = inject(DimmerService)
+  public modService = inject(ModalService)
 
   dimmer: boolean = false
   dimSubscription: Subscription | undefined
   private routerEvSubscription: Subscription | undefined
+  private userSubscription: Subscription | undefined
 
   ngOnInit() {
     // user session check
@@ -29,6 +33,12 @@ export class AppComponent implements OnInit, OnDestroy {
       const loggedinUser = JSON.parse(userJson)
       this.store.dispatch(SET_LOGGEDIN_USER({ user: loggedinUser }))
     } else this.store.dispatch(LOGOUT())
+
+    this.userSubscription = this.store.pipe(select(selectLoggedinUser)).subscribe(user => {
+      if (user && user.createdAt !== 0 && user.email && !user.isVerified) {
+        this.modService.openModal('user-auth')
+      }
+    })
 
     // dimmer management
     this.dimSubscription = this.dimService.dimmerSubject.subscribe((active: boolean) => {
@@ -51,5 +61,6 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     if (this.dimSubscription) this.dimSubscription.unsubscribe()
     if (this.routerEvSubscription) this.routerEvSubscription.unsubscribe()
+    if (this.userSubscription) this.userSubscription.unsubscribe()
   }
 }
--- frontend/src/app/cmps/modals/user-auth-modal/user-auth-modal.component.ts
+++ frontend/src/app/cmps/modals/user-auth-modal/user-auth-modal.component.ts
@@ -4,7 +4,7 @@ import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators }
 import { Store, select } from '@ngrx/store'
 import { AppState } from '../../../store/app.state'
 import { selectLoggedinUser } from '../../../store/user.selectors'
-import { EMPTY, Observable, Subscription, take } from 'rxjs'
+import { EMPTY, Observable, Subscription, debounceTime, filter, take } from 'rxjs'
 import { User } from '../../../models/user'
 import { UtilityService } from '../../../services/utility.service'
 import { EventBusService, showErrorMsg, showSuccessMsg } from '../../../services/event-bus.service'
@@ -27,27 +27,23 @@ export class UserAuthModalComponent implements OnInit, OnDestroy {
 
   verifyForm!: FormGroup
   verificationCode: string = ''
+  codeSent: boolean = false
+  message: string = 'Dear user, to ensure the safety of your account, we\'d like you to verify yourself.'
+  timer: number = 0
+  resendAvailable: boolean = false
+  private resendCodeTimer?: number
+  private closeModalTimer?: number
 
   ngOnInit() {
     this.loggedinUser$ = this.store.pipe(select(selectLoggedinUser))
 
     this.userSubscription = this.loggedinUser$.subscribe(user => {
       if (!user || user._id === '' || user.username === '') return
-      if (user.isVerified) this.closeModal()
-
-      else if (this.modService.isModalOpen('user-auth')) {
-        // Generate verification code
-        this.verificationCode = this.utilService.generateRandomCode()
-        const verifyFormData = {
-          code: this.verificationCode,
-          email: user.email,
-          username: user.username
-        }
-        // Send verification mail
-        this.utilService.sendVerificationMail(verifyFormData).subscribe({
-          error: (error) => console.error('Failed to send mail:', error)
-        })
+      if (this.modService.isModalOpen('user-auth')) {
         this.initializeForm()
+        this.closeModalTimer = setTimeout(() => {
+          this.closeModal()
+        }, 300000) as unknown as number
       }
     })
   }
@@ -56,6 +52,46 @@ export class UserAuthModalComponent implements OnInit, OnDestroy {
     this.verifyForm = this.fBuilder.group({
       code: ['', [Validators.required, this.codeValidator.bind(this)]]
     })
+    this.verifyForm.valueChanges.pipe(
+      filter(() => this.verifyForm.valid),
+      debounceTime(500)
+    ).subscribe(() => this.onSubmit())
+  }
+
+  sendCode() {
+    this.loggedinUser$.pipe(take(1)).subscribe(user => {
+      if (user && user._id && !user.isVerified) {
+        // Generate and send code
+        this.verificationCode = this.utilService.generateRandomCode()
+        const verifyFormData = {
+          code: this.verificationCode,
+          email: user.email,
+          username: user.username
+        }
+        this.utilService.sendVerificationMail(verifyFormData).subscribe({
+          next: () => {
+            this.codeSent = true
+            this.message = 'We\'ve sent the code to your email address, please insert it below.'
+            this.resendAvailable = false
+            this.startResendTimer()
+          },
+          error: (error) => console.error('Failed to send mail:', error)
+        })
+      }
+    })
+  }
+
+  startResendTimer() {
+    if (this.resendCodeTimer) clearInterval(this.resendCodeTimer)
+
+    this.timer = 60
+    this.resendCodeTimer = setInterval(() => {
+      if (this.timer > 0) this.timer--
+      else {
+        this.resendAvailable = true
+        clearInterval(this.resendCodeTimer)
+      }
+    }, 1000) as unknown as number
   }
 
   codeValidator(control: AbstractControl): ValidationErrors | null {
@@ -81,30 +117,29 @@ export class UserAuthModalComponent implements OnInit, OnDestroy {
 
   closeModal() {
     this.modService.closeModal('user-auth')
+    if (this.closeModalTimer) clearTimeout(this.closeModalTimer)
   }
 
   onSubmit() {
-    if (this.verifyForm.valid) {
-      if (this.verifyForm.value.code === this.verificationCode) {
-        this.loggedinUser$.pipe(take(1)).subscribe(user => {
-          if (user && user._id) {
-            const updatedUser: User = { ...user, isVerified: true }
-            this.store.dispatch(UPDATE_USER({ updatedUser }))
-            console.log('User verification updated')
-            showSuccessMsg('User Verified!',
-              'Thank you for your patience', this.eBusService)
-          }
-        })
-        this.closeModal()
-      } else {
-        console.error('Verification code does not match')
-        showErrorMsg('Verification failed!',
-          'Please try again later', this.eBusService)
-      }
+    if (this.verifyForm.value.code === this.verificationCode) {
+      this.loggedinUser$.pipe(take(1)).subscribe(user => {
+        if (user && user._id) {
+          const updatedUser: User = { ...user, isVerified: true }
+          this.store.dispatch(UPDATE_USER({ updatedUser }))
+          showSuccessMsg('User Verified!', 'Thank you for your patience',
+            this.eBusService)
+        }
+      })
+      this.closeModal()
+    } else {
+      showErrorMsg('Verification failed!', 'Please try again later',
+        this.eBusService)
     }
   }
 
   ngOnDestroy() {
     if (this.userSubscription) this.userSubscription.unsubscribe()
+    if (this.resendCodeTimer) clearInterval(this.resendCodeTimer)
+    if (this.closeModalTimer) clearTimeout(this.closeModalTimer)
   }
 }
