c3faaef9ad426d6e7e168a64604a34c9065899b4
TAC42 <tomi.carlin@gmail.com>
2024-01-28 17:19:30 +0200

    finished working on admin guard

--- frontend/src/app/guards/admin.guard.ts
+++ frontend/src/app/guards/admin.guard.ts
@@ -1,25 +1,49 @@
-// import { Injectable } from '@angular/core';
-// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-// import { AuthService } from './auth.service'; // replace with your auth service or user service
+import { Injectable, inject } from '@angular/core'
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router'
+import { UserService } from '../services/user.service'
+import { selectLoggedinUser, selectUser } from '../store/user.selectors'
+import { Store } from '@ngrx/store'
+import { AppState } from '../store/app.state'
+import { Observable, of } from 'rxjs'
+import { switchMap, map, catchError } from 'rxjs/operators'
+import { LOAD_USER } from '../store/user.actions'
+import { User } from '../models/user'
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AdminGuard implements CanActivate {
-//   constructor(private authService: AuthService, private router: Router) {}
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  private store = inject(Store<AppState>)
+  private router = inject(Router)
+  private uService = inject(UserService)
 
-//   canActivate(
-//     route: ActivatedRouteSnapshot,
-//     state: RouterStateSnapshot
-//   ): boolean | UrlTree {
-//     // Check if user is an admin (assuming your AuthService has a method to check user role)
-//     const isAdmin = this.authService.isUserAdmin(); // Adjust this based on your AuthService implementation
-
-//     if (isAdmin) {
-//       return true; // Allow access for admin users
-//     } else {
-//       // Redirect unauthorized users to a different route or show an error
-//       return this.router.createUrlTree(['/']); // Redirect to home or another route
-//     }
-//   }
-// }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | UrlTree | Observable<boolean | UrlTree> {
+    return this.store.select(selectLoggedinUser).pipe(
+      switchMap(user => {
+        if (user?._id) {
+          this.store.dispatch(LOAD_USER({ userId: user._id }))
+          return this.store.select(selectUser).pipe(
+            map((newUser: User) => {
+              if (newUser && newUser.isAdmin) {
+                return true
+              } else {
+                console.log('WARNING UNAUTHORIZED USER!')
+                return this.router.createUrlTree(['/'])
+              }
+            })
+          )
+        } else {
+          console.log('User._id is falsy. Navigating away.')
+          return of(this.router.createUrlTree(['/']))
+        }
+      }),
+      catchError(error => {
+        console.error('Error checking user:', error)
+        return of(this.router.createUrlTree(['/']))
+      })
+    );
+  }
+}
