bd56a475f84b4718e881453359c3b15d1c6c8f73
TAC42 <tomi.carlin@gmail.com>
2024-02-07 23:01:40 +0200

    fixed admin.guard bug that wont open links after admin tries for the first time

--- frontend/src/app/guards/admin.guard.ts
+++ frontend/src/app/guards/admin.guard.ts
@@ -2,12 +2,13 @@ import { Injectable, inject } from '@angular/core'
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router'
 import { UserService } from '../services/user.service'
 import { selectLoggedinUser, selectUser } from '../store/user.selectors'
-import { Store } from '@ngrx/store'
+import { ActionsSubject, Store } from '@ngrx/store'
 import { AppState } from '../store/app.state'
 import { Observable, of } from 'rxjs'
-import { switchMap, map, catchError } from 'rxjs/operators'
-import { LOAD_USER } from '../store/user.actions'
+import { switchMap, map, catchError, take, filter } from 'rxjs/operators'
+import { LOAD_USER, SET_USER } from '../store/user.actions'
 import { User } from '../models/user'
+import { ofType } from '@ngrx/effects'
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,7 @@ export class AdminGuard implements CanActivate {
   private store = inject(Store<AppState>)
   private router = inject(Router)
   private uService = inject(UserService)
+  private actions$ = inject(ActionsSubject)
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -25,8 +27,16 @@ export class AdminGuard implements CanActivate {
       switchMap(user => {
         if (user?._id) {
           this.store.dispatch(LOAD_USER({ userId: user._id }))
-          return this.store.select(selectUser).pipe(
+          // Wait for SET_USER action
+          return this.actions$.pipe(
+            ofType(SET_USER),
+            take(1),
+            switchMap(() => this.store.select(selectUser)), 
+            filter(newUser => !!newUser && !!newUser._id), 
+            take(1), 
             map((newUser: User) => {
+              console.log('selectUSER: ', newUser)
+
               if (newUser && newUser.isAdmin) {
                 return true
               } else {
@@ -44,6 +54,6 @@ export class AdminGuard implements CanActivate {
         console.error('Error checking user:', error)
         return of(this.router.createUrlTree(['/']))
       })
-    );
+    )
   }
 }
