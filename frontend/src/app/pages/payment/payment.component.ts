a9573ebc3f16c13c2339531742f0c0dbc9b11f19
TAC42 <tomi.carlin@gmail.com>
2024-02-13 00:08:22 +0200

    working on payment layout

--- frontend/src/app/pages/payment/payment.component.ts
+++ frontend/src/app/pages/payment/payment.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'payment',
   templateUrl: './payment.component.html'
 })
 export class PaymentComponent {
+  @HostBinding('class.full') fullClass = true
+  @HostBinding('class.w-h-100') fullWidthHeightClass = true
+  @HostBinding('class.layout-row') layoutRowClass = true
+  optionState: string = ''
 
+  setSelection(option: string){
+    this.optionState = option
+  }
 }
