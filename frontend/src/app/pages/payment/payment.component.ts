85c6d9f1a606f11997cbd40055a88e0f299e3863
TAC42 <tomi.carlin@gmail.com>
2024-02-15 17:40:06 +0200

    working on the payment layout and forms logic

--- frontend/src/app/pages/payment/payment.component.ts
+++ frontend/src/app/pages/payment/payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostBinding } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'payment',
@@ -9,6 +10,19 @@ export class PaymentComponent {
   @HostBinding('class.w-h-100') fullWidthHeightClass = true
   @HostBinding('class.layout-row') layoutRowClass = true
   optionState: string = ''
+  payType: string = 'venmo'
+  
+  paymentForm: FormGroup;
+  
+  constructor(private fb: FormBuilder) {
+    this.paymentForm = this.fb.group({
+      paymentMethod: ['venmo']
+    })
+
+    this.paymentForm.get('paymentMethod')?.valueChanges.subscribe(value => {
+      this.payType = value
+    })
+  }
 
   setSelection(option: string){
     this.optionState = option
