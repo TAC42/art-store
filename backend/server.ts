fb46767348a6b197bdb34dafe8e5af5e7c075afa
TAC42 <tomi.carlin@gmail.com>
2024-03-17 11:43:29 +0200

    deploying troubleshoot with Netlify

--- backend/server.ts
+++ backend/server.ts
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
     origin: [
       'http://oricarlin.onrender.com',
       'https://oricarlin.com',
-      'https://oricarlin.netlify.app',
+      'https://main--oricarlin.netlify.app',
       'http://127.0.0.1:4200',
       'http://localhost:4200',
       'http://127.0.0.1:3030',
